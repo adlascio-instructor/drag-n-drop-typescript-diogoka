2860e0e65bdc938a73133939af5546e78eb59c68
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-06-13 09:47:36 -0700

    Step 03 ok.

--- src/components/ProjectInput.ts
+++ src/components/ProjectInput.ts
@@ -34,10 +34,8 @@ class ProjectInput {
         this.attach();
         this.titleInputElement = document.getElementById('title')! as HTMLInputElement;
         this.descriptionInputElement = document.getElementById('description')! as HTMLInputElement;
-        this.peopleInputElement = document.getElementById('people')! as HTMLInputElement;
-        console.log(this.titleInputElement);
-        
-    }
+        this.peopleInputElement = document.getElementById('people')! as HTMLInputElement;          
+}
 
     private attach() {
         this.hostElement.append(this.element);
@@ -51,8 +49,34 @@ class ProjectInput {
     @Autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        let userInput = this.gatherUserInput();
+        console.log("userInputArray", userInput);
+        if(Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log("title", title);
+            console.log("description", description);
+            console.log("people", people);
+            this.clearInputs();
+        }
+        
 
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        if(this.titleInputElement.value.trim().length === 0 || this.descriptionInputElement.value.trim().length === 0 || this.peopleInputElement.value.trim().length === 0) {
+            alert('Invalid input, please try again');
+            return;
+        }
+        let enteredTitle = this.titleInputElement.value;
+        let enteredDescription = this.descriptionInputElement.value;
+        let enteredPeople = this.peopleInputElement.value;
+        return [enteredTitle, enteredDescription, +enteredPeople];             
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
     }
 }
 
